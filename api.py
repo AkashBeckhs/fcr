0b637c6cae14375a369d0356b04d5acfc3e9225d
Akash <akash.insignia@gmail.com>
2019-06-24 15:33:20 +0530

    added try catch

--- api.py
+++ api.py
@@ -14,16 +14,19 @@ def home():
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      checkImage = request.files['check']
-      verifyImage = request.files['verify']
-      if checkImage== None or verifyImage==None:
+      try:
+         checkImage = request.files['check']
+         verifyImage = request.files['verify']
+         if checkImage== None or verifyImage==None:
           return "Please provide two images"
-      checkImage.filename="checkImage.jpg"
-      verifyImage.filename="verifyImage.jpg"
-      checkImage.save(secure_filename(checkImage.filename))
-      verifyImage.save(secure_filename(verifyImage.filename))
-      res=fc.checkImage(checkImage,verifyImage)
-      return str(res)
+         checkImage.filename="checkImage.jpg"
+         verifyImage.filename="verifyImage.jpg"
+         checkImage.save(secure_filename(checkImage.filename))
+         verifyImage.save(secure_filename(verifyImage.filename))
+         res=fc.checkImage(checkImage,verifyImage)
+         return str(res)
+      except Exception as e:
+         return "error occured , Maybe you have not provided proper images."
 		
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=8080,debug = True)
