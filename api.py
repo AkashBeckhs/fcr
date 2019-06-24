8ca9660c8b2e87f18c551c21374929bcc906e27b
Akash <akash.insignia@gmail.com>
2019-06-24 16:29:16 +0530

    result page added

--- api.py
+++ api.py
@@ -18,15 +18,15 @@ def upload_file():
          checkImage = request.files['check']
          verifyImage = request.files['verify']
          if checkImage== None or verifyImage==None:
-          return "Please provide two images"
+          return render_template("result.html",res="Please provide two images")
          checkImage.filename="checkImage.jpg"
          verifyImage.filename="verifyImage.jpg"
          checkImage.save(secure_filename(checkImage.filename))
          verifyImage.save(secure_filename(verifyImage.filename))
          res=fc.checkImage(checkImage,verifyImage)
-         return str(res)
+         return render_template("result.html",res=res[0])
       except Exception as e:
-         return "error occured , Maybe you have not provided proper images."
+         return render_template("result.html",res="There was some error in comparing the given images")
 		
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=8080,debug = True)
