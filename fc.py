49b6945c247f2cd2432e426fabff9937586b660a
AKASH Akash <akash.akash@soprasteria.com>
2019-06-26 12:38:40 +0530

    reduced tolerence to 0.52

--- fc.py
+++ fc.py
@@ -6,5 +6,5 @@ def checkImage(img1,img2):
     unknown_image = face_recognition.load_image_file(img2)
     biden_encoding = face_recognition.face_encodings(known_image)[0]
     unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+    results = face_recognition.compare_faces([biden_encoding], unknown_encoding,tolerance=0.52)
     return results
