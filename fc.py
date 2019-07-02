bb01ca4048115504346520d718c3abafae165072
AKASH Akash <akash.akash@soprasteria.com>
2019-07-02 10:30:44 +0530

    Jitters = 20

--- fc.py
+++ fc.py
@@ -4,7 +4,7 @@ import face_recognition
 def checkImage(img1,img2):
     known_image = face_recognition.load_image_file(img1)
     unknown_image = face_recognition.load_image_file(img2)
-    biden_encoding = face_recognition.face_encodings(known_image,num_jitters=100)[0]
-    unknown_encoding = face_recognition.face_encodings(unknown_image,num_jitters=100)[0]
+    biden_encoding = face_recognition.face_encodings(known_image,num_jitters=20)[0]
+    unknown_encoding = face_recognition.face_encodings(unknown_image,num_jitters=20)[0]
     results = face_recognition.compare_faces([biden_encoding], unknown_encoding,tolerance=0.52)
     return results
