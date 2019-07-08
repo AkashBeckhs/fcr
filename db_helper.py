d7f6681a7117cd9d88c0e77e04d00480c5f5f5da
Akash <akash.insignia@gmail.com>
2019-07-08 10:28:41 +0530

    Inserting encoding

--- db_helper.py
+++ db_helper.py
@@ -1,6 +1,5 @@
 import psycopg2 as ps
 
-
 host='127.0.0.1'
 port="5432"
 db="test"
@@ -10,3 +9,16 @@ password="admin"
 def getDbObject():
     connection=ps.connect(database=db, user=user, password=password, host=host, port=port)
     return connection
+
+
+def insert(img1):
+    try:
+        conn=getDbObject()
+        cursor=conn.cursor()
+        sql="insert into vectors(vector) values(cube(array["+str(img1)+"]))"
+        print(sql)
+        cursor
+    except Exception as e:
+        print("Exception while saving vector "+str(e))
+
+   
