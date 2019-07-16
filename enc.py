c58227177a35d0707a2a17738c73de19825f5aca
Akash <akash.insignia@gmail.com>
2019-07-16 15:32:27 +0530

    added cipher to QR code

--- enc.py
+++ enc.py
@@ -7,18 +7,19 @@ from Crypto.Cipher import AES
 
 BS = 16
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s : s[0:-ord(s[-1])]
+unpad = lambda s : s[0:-s[-1]]
+
 
 class AESCipher:
 
     def __init__( self, key ):
-        self.key = key
+        self.key = hashlib.sha256(key.encode('utf-8')).digest()
 
     def encrypt( self, raw ):
         raw = pad(raw)
         iv = Random.new().read( AES.block_size )
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw ))
+        return base64.b64encode( iv + cipher.encrypt( raw ) )
 
     def decrypt( self, enc ):
         enc = base64.b64decode(enc)
