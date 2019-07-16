cd947aede05fc9ecc318a7a86588e7d806edbcca
Akash <akash.insignia@gmail.com>
2019-07-16 14:40:34 +0530

    added cipher to QR code

--- enc.py
+++ enc.py
@@ -3,25 +3,27 @@ import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
 
-BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s : s[0:-s[-1]]
+class AESCipher(object):
 
-class AESCipher:
+    def __init__(self, key): 
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
 
-    def __init__( self, key ):
-        self.key = hashlib.sha256(key.encode('utf-8')).digest()
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
 
-    def encrypt( self, raw ):
-        raw = pad(raw)
-        iv = Random.new().read( AES.block_size )
-        cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw ) )
-
-    def decrypt( self, enc ):
+    def decrypt(self, enc):
         enc = base64.b64decode(enc)
-        iv = enc[:16]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return unpad(cipher.decrypt( enc[16:] ))
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
 
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
 
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
