61a5a91c91147a1c1f5e3272553ff79010baa0d3
Akash <akash.insignia@gmail.com>
2019-07-16 15:30:06 +0530

    added cipher to QR code

--- enc.py
+++ enc.py
@@ -18,10 +18,16 @@ class AESCipher:
         raw = pad(raw)
         iv = Random.new().read( AES.block_size )
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw ) )
+        return base64.b64encode( iv + cipher.encrypt( raw ))
 
     def decrypt( self, enc ):
         enc = base64.b64decode(enc)
         iv = enc[:16]
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
         return unpad(cipher.decrypt( enc[16:] ))
+
+cipher = AESCipher('mysecretpassword')
+encrypted = cipher.encrypt('Secret Message A')
+decrypted = cipher.decrypt(encrypted)
+print(encrypted)
+print(decrypted)
