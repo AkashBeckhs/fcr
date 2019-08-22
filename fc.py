53b121707a9858201b1db2f5531b5e95c0965432
root <root@downloader.us-central1-a.c.master-isotope-244304.internal>
2019-08-22 10:10:55 +0530

    Change IP on upload.html page and have create new database

--- fc.py
+++ fc.py
@@ -5,7 +5,7 @@ import db_helper as db
 def checkImage(img1,img2):
     known_image = face_recognition.load_image_file(img1)
     unknown_image = face_recognition.load_image_file(img2)
-    biden_encoding = face_recognition.face_encodings(known_image,num_jitters=50)[0]
+    biden_encoding = face_recognition.face_encodings(known_image,num_jitters=5)[0]
     unknown_encoding = face_recognition.face_encodings(unknown_image,num_jitters=1)[0]
     results = face_recognition.compare_faces([biden_encoding], unknown_encoding,tolerance=0.52)
     return results,biden_encoding
